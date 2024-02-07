1f6d1e355300d65255a08ebc941fd79313e49e5a
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2024-02-07 22:57:22 +0200

    genres refactorerd as static data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
-import { Genre } from "../instances";
-import useData from "./useData";
+import genres from '../data/genres'
 
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({data: genres, isLoading: false, error: null});
 
 export default useGenres;
