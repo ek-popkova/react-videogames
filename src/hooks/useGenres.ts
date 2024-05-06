b416d381c82bf45acf58bd4d7660e134fcdfc47c
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2024-05-06 21:55:53 +0300

    genres refactored to use react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,16 @@
-import genres from '../data/genres'
+import { useQuery } from '@tanstack/react-query';
+import { FetchResponse, Genre } from './../instances';
+import apiClient from '../services/api-client';
+import genres from '../data/genres';
 
-const useGenres = () => ({data: genres, isLoading: false, error: null});
+const useGenres = () => 
+    useQuery(
+        {
+            queryKey: ['genres'],
+            queryFn: () => apiClient.get<FetchResponse<Genre>>('/genres').then((res) => res.data),
+            staleTime: 24 * 60 * 60 * 1000, //24h
+            initialData: { count: genres.length, results: genres }
+        }
+);
 
 export default useGenres;
