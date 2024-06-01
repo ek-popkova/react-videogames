6953b6e277d31e4284428cf9749289de52837398
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2024-06-01 21:21:14 +0300

    hooks refactored to use query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,16 +1,32 @@
-import { Game, GameQuery, Genre, Platform } from "../instances";
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse, Game, GameQuery} from "../instances";
+import apiClient from "../services/api-client";
 
 const useGames = (gameQuery: GameQuery) =>
-    useData<Game>(
-        '/games',
-        {
-            params: {
-                genres: gameQuery.genre?.id,
-                parent_platforms: gameQuery.platform?.id,
-                ordering: gameQuery.sortOrder,
-                search: gameQuery.searchText
-            }
-        }, [gameQuery]);
+    useQuery<FetchResponse<Game>, Error>({
+        queryKey: ['games', gameQuery],
+        queryFn: () =>
+            apiClient
+                .get<FetchResponse<Game>>('/games', {
+                    params: {
+                        genres: gameQuery.genre?.id,
+                        parent_platforms: gameQuery.platform?.id,
+                        ordering: gameQuery.sortOrder,
+                        search: gameQuery.searchText
+                    },
+                })
+                .then(res => res.data)
+    })
+    // useData<Game>(
+    //     '/games',
+    //     {
+    //         params: {
+    //             genres: gameQuery.genre?.id,
+    //             parent_platforms: gameQuery.platform?.id,
+    //             ordering: gameQuery.sortOrder,
+    //             search: gameQuery.searchText
+    //         }
+    //     }, [gameQuery]);
+
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,17 @@
-import { Platform } from "../instances";
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse, Platform } from "../instances";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+//const usePlatforms = () => useData<Platform>('/platforms/lists/parents'); - old approach with useData
+
+const usePlatforms = () => useQuery(
+    {
+        queryKey: ['platforms'],
+        queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then((res) => res.data),
+        staleTime: 24 * 60 * 60 * 1000, //24h
+        initialData: platforms 
+    }
+);
 
 export default usePlatforms;
