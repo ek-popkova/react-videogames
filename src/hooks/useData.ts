ecd7af8df229b565d204500d591973943d48ee0b
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2024-06-08 20:37:58 +0300

    empty file fix to deploy

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,31 +1,41 @@
-// import { useEffect, useState } from "react";
-// import { FetchResponse } from "../instances";
-// import apiClient from "../services/api-client";
-// import { AxiosRequestConfig, CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import { FetchResponse } from "../instances";
+import axios, { AxiosRequestConfig, CanceledError } from "axios";
 
-// const useData =<T>(endpoint:string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
-//         const [data, setData] = useState<T[]>([]);
-//         const [error, setError] = useState("");
-//         const [isLoading, setLoading] = useState(false);
+//this is an obsole hook, that is not used in the app anymore
+//keep it just as an example of code without React Query
 
-//         useEffect(() => {
-//           const controller = new AbortController();
-//           setLoading(true);
-//           apiClient
-//             .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
-//             .then((res) => {
-//               setData(res.data.results);
-//               setLoading(false);
-//             })
-//             .catch((err) => {
-//               if (err instanceof CanceledError) return;
-//               setError(err.message);
-//               setLoading(false);
-//             });
+const apiClient = axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: import.meta.env.VITE_API_KEY
+    },
+});
 
-//           return () => controller.abort();
-//         }, deps ? [...deps] : []);
-//         return { data, error, isLoading };
-// }
+const useData =<T>(endpoint:string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
+        const [data, setData] = useState<T[]>([]);
+        const [error, setError] = useState("");
+        const [isLoading, setLoading] = useState(false);
 
-// export default useData;
+        useEffect(() => {
+          const controller = new AbortController();
+          setLoading(true);
+          apiClient
+            .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
+            .then((res) => {
+              setData(res.data.results);
+              setLoading(false);
+            })
+            .catch((err) => {
+              if (err instanceof CanceledError) return;
+              setError(err.message);
+              setLoading(false);
+            });
+
+          return () => controller.abort();
+        }, deps ? [...deps] : []);
+        return { data, error, isLoading };
+}
+
+export default useData;
+
